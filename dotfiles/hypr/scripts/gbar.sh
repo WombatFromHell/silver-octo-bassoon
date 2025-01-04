435f307ed1b1d8aba44118ab53c244d489153194
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-01-03 20:46:31 -0700

    Depend on UWSM more and implemented proper lock/suspend support

--- dotfiles/hypr/scripts/gbar.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if pgrep -x "gBar" >/dev/null; then
-	pkill -f gBar
-fi
-
-uwsm app -- gBar bar 0 &
--- dotfiles/hypr/scripts/sys.sh
+++ dotfiles/hypr/scripts/sys.sh
@@ -8,24 +8,84 @@ then
 	PREFIX="uwsm app --"
 fi
 
-hypr_exit() {
-	if pgrep -x "Hyprland" &>/dev/null; then
-		$PREFIX hyprctl dispatch exit
-	fi
-}
-lock() {
-	$PREFIX hyprlock &
-}
-sys_suspend() {
-	lock
-	disown
-	systemctl suspend
+close_steam() {
+	pkill -SIGTERM -x "steam"
+	current_time=$(date +%s)
+	start_time=$(date +%s)
+	# limit waiting to 30s
+	max_wait=30
+	while true; do
+		COUNT=$(pgrep "steam" | wc -l)
+		if [ "$COUNT" -eq 0 ]; then
+			return
+		fi
+
+		current_time=$(date +%s)
+		elapsed_time=$((current_time - start_time))
+		if [ "$elapsed_time" -ge "$max_wait" ]; then
+			return
+		fi
+		sleep 1
+	done
 }
 
+graceful_exit() {
+	notify-send "System" "Gracefully closing apps..."
+
+	# close layers
+	systemctl --user stop gBar.service
+	systemctl --user stop pipewire.service
+	systemctl --user stop wireplumber.service
+	sleep 1
+
+	# close all client windows
+	HYPRCMDS=$(hyprctl --instance 0 -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
+	hyprctl --instance 0 --batch "$HYPRCMDS" >/tmp/hyprgraceexit.log 2>&1
+	close_steam
+
+	COUNT=$(hyprctl clients | grep -c "class:")
+	if [ "$COUNT" -eq "0" ]; then
+		sleep 1
+		return
+	else
+		notify-send "System" "Some apps didn't close. Aborting exit."
+		systemctl --user start gBar.service
+		systemctl --user start pipewire.service
+		systemctl --user start wireplumber.service
+		sleep 1
+		exit 1
+	fi
+}
+
+hypr_exit() {
+	if pgrep -x "Hyprland" >/dev/null; then
+		graceful_exit
+		if [ -n "$PREFIX" ]; then
+			uwsm stop
+			sleep 1
+			hyprctl --instance 0 dispatch exit
+		else
+			hyprctl --instance 0 dispatch exit
+		fi
+	fi
+}
+
+hypr_shutdown() {
+	graceful_exit
+	systemctl poweroff
+}
+
+# MAIN #
 if [ "$1" == "lock" ]; then
-	lock
+	$PREFIX hyprlock --immediate &
+	sleep 3
+	hyprctl dispatch dpms off
+elif [ "$1" == "softlock" ] && pgrep -x "hypridle"; then
+	loginctl lock-session
 elif [ "$1" == "exit" ]; then
 	hypr_exit
 elif [ "$1" == "suspend" ]; then
-	sys_suspend
+	systemctl suspend
+elif [ "$1" == "shutdown" ]; then
+	hypr_shutdown
 fi
