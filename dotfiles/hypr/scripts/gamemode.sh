983c967daa9b5b7955d60a013b6f54dcc9c143b3
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-01-05 10:05:08 -0700

    Improved gamemode.sh and gamescope support on Hyprland

--- dotfiles/hypr/scripts/gamemode.sh
+++ dotfiles/hypr/scripts/gamemode.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
+# make sure to:
+# sudo setcap 'cap_sys_nice=eip' /usr/bin/gamescope
+
+get_preferred_resolution() {
+	output=$1
+	if ! wlr-randr --output "$output" | grep -q "Enabled: yes"; then
+		echo "Error: no enabled output found!"
+		exit 1
+	fi
+	wlr-randr | grep -A 1 "Modes:" | grep "preferred" |
+		sed -E 's/[[:space:]]+([0-9]+)x([0-9]+) px, ([0-9.]+) Hz.*$/\1 \2 \3/' |
+		awk '{printf "%d %d %.0f\n", $1, $2, $3}'
+}
+
+OUTPUT="DP-3"
+GPR=$(get_preferred_resolution "$OUTPUT")
+WIDTH=$(echo "$GPR" | cut -d' ' -f1)
+HEIGHT=$(echo "$GPR" | cut -d' ' -f2)
+#RATE=$(echo "$GPR" | cut -d' ' -f3)
+
+#PERFUTIL="gamemoderun"
+PERFUTIL="game-performance"
+GAMESCOPE=$(which gamescope)
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-if [ "$HYPRGAMEMODE" = 1 ]; then
+if [ "$HYPRGAMEMODE" -eq 1 ]; then
 	hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:shadow:enabled 0;\
@@ -10,6 +33,11 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
-	exit
+	$PERFUTIL "$GAMESCOPE" \
+		-W "$WIDTH" -H "$HEIGHT" \
+		--backend sdl \
+		--hdr-enabled \
+		--immediate-flips \
+		-f "$@"
 fi
 hyprctl reload
