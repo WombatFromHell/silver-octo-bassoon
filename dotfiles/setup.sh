142524e225428f347a7f6c6090cd700e54cfce20
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-01-14 15:33:04 -0700

    Some bug fixes for dotfiles setup.sh

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+OS=$(uname)
 # Ensure script runs from its directory
 script_dir="$(dirname "$(readlink -f "$0")")"
 cd "$script_dir" || exit 1
 
-# Help message
 show_help() {
   echo "Usage: $(basename "$0") [-a|--all] [-h|--help]"
   echo "Options:"
@@ -12,17 +12,29 @@ show_help() {
   echo "  -h, --help    Show this help message"
   exit 0
 }
-
-# Parse arguments
 [[ "$1" == "-h" || "$1" == "--help" ]] && show_help
 AUTO_CONFIRM=false
 [[ "$1" == "-a" || "$1" == "--all" ]] && AUTO_CONFIRM=true
 
-# Confirmation function
+process_perms() {
+  find . -type d -exec chmod 0755 {} \;
+  find . -type f -exec chmod 0644 {} \;
+  find . -regex ".*\(\.tmux\|\.sh\|/tpm\)" -exec chmod 0755 {} \;
+  echo -e "\nFixed repo permissions..."
+}
+
 confirm() {
   [[ "$AUTO_CONFIRM" == true ]] && return 0
   read -r -p "$1 (y/N) " response
   [[ "$response" == "y" || "$response" == "Y" ]]
+}
+
+check_for_linux() {
+  if [ "$OS" != "Linux" ]; then
+    return 1
+  else
+    return 0
+  fi
 }
 
 unlink_dir() {
@@ -34,7 +46,6 @@ unlink_dir() {
 
 }
 
-# Special directory handlers
 handle_home() {
   local files=(".bashrc" ".zshrc" ".wezterm.lua")
   for file in "${files[@]}"; do
@@ -65,16 +76,30 @@ handle_nix() {
   stow nix
 }
 
-handle_tmux() {
-  echo "Attempting to correct tmux source permissions..."
-  find "./$1"/ -type d -exec chmod 0755 {} \;
-  find "./$1"/ -type f -exec chmod 0644 {} \;
-  find "./$1"/ -type f -name "*.tmux" -exec chmod 0755 {} \;
-  find "./$1"/ -type f -name "*.sh" -exec chmod 0755 {} \;
-  find "./$1"/ -type f -name "tpm" -exec chmod 0755 {} \;
+handle_stow() {
+  local dir=$1
+  local target=$2
+
+  # exclusions from normal stowing...
+  if [ "$dir" == "systemd" ] && ! check_for_linux; then
+    echo -e "\nSkipping $dir stow on $OS..."
+    return
+  fi
+
+  if confirm "Removing all files from $target before stowing"; then
+    unlink_dir "$target"
+    mkdir -p "$target"/
+    stow "$dir"
+    echo -e "\n'$dir' has been stowed!"
+
+    # Post-stow actions
+    case "$dir" in
+    fish) fish -c "fisher update" ;;
+    bat) bat cache --build ;;
+    esac
+  fi
 }
 
-# Main stow function
 stow_directory() {
   local dir=$1
   local target="$HOME/.config/$dir"
@@ -87,30 +112,23 @@ stow_directory() {
     confirm "Are you sure you want to stow $dir?" && handle_scripts "$dir"
     ;;
   pipewire)
-    confirm "Are you sure you want to stow $dir?" && handle_pipewire "$dir"
+    if check_for_linux; then
+      confirm "Are you sure you want to stow $dir?" && handle_pipewire "$dir"
+    else
+      echo -e "\nSkipping $dir stow on $OS..."
+    fi
     ;;
   nix)
     confirm "Are you sure you want to stow $dir?" && handle_nix
     ;;
   *)
-    if confirm "Removing all files from $target before stowing"; then
-      unlink_dir "$target"
-      mkdir -p "$target"/
-      stow "$dir"
-      echo -e "\n'$dir' has been unstowed!"
-
-      # Post-stow actions
-      case "$dir" in
-      fish) fish -c "fisher update" ;;
-      bat) bat cache --build ;;
-      tmux) handle_tmux "$dir" ;;
-      esac
-    fi
+    handle_stow "$dir" "$target"
     ;;
   esac
 }
 
-# Main execution
+# fix repo perms before stowing (just in case)
+process_perms
 mapfile -t directories <sources.txt
 for dir in "${directories[@]}"; do
   stow_directory "$dir"
