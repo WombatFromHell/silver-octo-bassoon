5dd663d5e82b19a62ff059abfdb50a98ea45a013
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-03-04 18:47:52 -0700

    Include a workaround for gpg/pinentry in distrobox ini

--- dotfiles/sync.sh
+++ dotfiles/sync.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 LOCAL="."
-# REMOTE="$HOME/Backups/linux-config/backups/dotfiles/"
-REMOTE="$HOME/Backups/dotfiles/"
+REMOTE="$HOME/Backups/linux-config/backups/dotfiles/"
 
 _CMD=(rsync -avL --checksum --update)
 EXCLUDES=(
