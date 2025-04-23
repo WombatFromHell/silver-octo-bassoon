74d151bdc2022933bdd7d8122dc008a536bf873d
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-04-23 09:07:14 -0600

    Revamped fix-wakeups.sh support script

--- roles/base/files/bin/fix-wakeups.sh
+++ roles/base/files/bin/fix-wakeups.sh
@@ -1,2 +1,16 @@
-#!/bin/sh
-echo GPP0 >/proc/acpi/wakeup
+#!/usr/bin/env bash
+
+devices=(
+	GPP0 # PCIe bridge
+	XH00 # Disable mouse/keyboard hub wakeups
+)
+
+for dev in "${devices[@]}"; do
+	if grep -q "$dev.*enabled" /proc/acpi/wakeup; then
+		if ! echo "$dev" | sudo tee /proc/acpi/wakeup >/dev/null; then
+			echo "Error: Failed to disable $dev" >&2
+		else
+			echo "Disabling ACPI wakeups on: $dev"
+		fi
+	fi
+done
