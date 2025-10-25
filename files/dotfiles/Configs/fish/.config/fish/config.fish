aaa1d89dc2f1202cbd561b2e6b6eadc261195713
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-10-25 08:23:50 -0600

    Add a fish alias for tpm's 'update_plugins' script

--- files/dotfiles/Configs/fish/.config/fish/config.fish
+++ files/dotfiles/Configs/fish/.config/fish/config.fish
@@ -327,6 +327,8 @@ alias nixosopt='sudo nix-store --gc && sudo nix-store --optimize'
 alias nixopt='nix-store --gc && nix-store --optimize'
 #
 alias gpgfix='gpgconf -K all && gpgconf --launch gpg-agent'
+#
+alias update_tmux='~/.config/tmux/plugins/tpm/bin/update_plugins all'
 
 set NIX_DAEMON_FISH_SRC /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
 if test -z $NIX_PROFILES; and test -r "$NIX_DAEMON_FISH_SRC"
