6fdba0d04124189a4ae479f5e0c0359b82e47a29
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-11-13 19:36:36 -0700

    Add a helper script for cleanup of temp files in ansible library project dir

---
+++ library/clean.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# prevent script from being run outside the project directory
+script_dir="$(dirname "$(readlink -f "$0")")"
+if [[ "$(pwd -P)" != "$script_dir" ]]; then
+  echo "Error: script must be run from the project directory!"
+  exit 1
+fi
+
+rm -rf ./.venv/ ./.ansible/ ./.pytest_cache/ ./.ruff_cache/ ./__pycache__/ ./tests/__pycache__/ ./.coverage
