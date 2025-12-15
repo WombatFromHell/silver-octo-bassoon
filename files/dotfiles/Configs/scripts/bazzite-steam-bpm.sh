0b4019701552cf7c63b6ed0b32485e192a645c18
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-12-15 14:12:37 -0700

    Update bpm helper to allow for conditional wrappers

--- files/dotfiles/Configs/scripts/bazzite-steam-bpm.sh
+++ files/dotfiles/Configs/scripts/bazzite-steam-bpm.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+add_if_exists() {
+  local array_name="$1"
+  local file="$2"
+  if [[ -f "$file" ]]; then
+    eval "$array_name+=(\"$file\")"
+  fi
+}
+
 SCRIPTS="$HOME/.local/bin/scripts"
 #
 # STEAM=$(which steam)
@@ -12,20 +20,28 @@ GAMESCOPE_WRAPPER="$SCRIPTS/nscb.pyz"
 GAMESCOPE_ARGS=(
   -p std
   -p hdr
+  --mangoapp
   -e
   --
 )
 ENV_VARS=(
   env
+  PROTON_USE_NTSYNC=1
   PROTON_ENABLE_WAYLAND=1
   DXVK_FRAME_RATE=72
   VKD3D_FRAME_RATE=72
+  MESA_VK_WSI_PRESENT_MODE="mailbox"
 )
-PRE_WRAPPER="$SCRIPTS/perfboost.sh"
+
+# include some optional wrappers conditionally
+OTHER_WRAPPERS=()
+add_if_exists "OTHER_WRAPPERS" "$HOME/mesa/mesa-run.sh"
+add_if_exists "OTHER_WRAPPERS" "$SCRIPTS/perfboost.sh"
+
 #
 CMD=(
   "${ENV_VARS[@]}"
-  "${PRE_WRAPPER}"
+  "${OTHER_WRAPPERS[@]}"
   "${GAMESCOPE_WRAPPER}" "${GAMESCOPE_ARGS[@]}"
   "${STEAM}" "${STEAM_ARGS[@]}"
 )
