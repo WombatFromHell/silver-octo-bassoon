f16b79cae50bc66cbcd5063c17e62dfcbccf7964
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-03-17 19:44:28 -0600

    Support 'trash-cli' with fzf enabled fish helper functions

--- files/dotfiles/Configs/fish/.config/fish/config.fish
+++ files/dotfiles/Configs/fish/.config/fish/config.fish
@@ -246,6 +246,51 @@ alias sudoe='sudo -E env PATH=(string join ':' $PATH)'
 alias _rsync='rsync -avL --partial --update'
 alias _rsyncd='_rsync --dry-run'
 #
+if command -q trash
+    alias rm='trash-put'
+    alias rmd='trash-put'
+    alias rmc='trash-empty'
+    alias rmr='trash-restore'
+
+    function rmls
+        if not command -q fzf
+            trash-list
+            return
+        end
+
+        set -l selected (
+            trash-list \
+            | sort -r \
+            | fzf --multi \
+                  --prompt='Restore> ' \
+                  --header='TAB to select, ENTER to restore, ESC to cancel' \
+                  --preview-window=hidden \
+            | awk '{print $3}'
+        )
+
+        if test -z "$selected"
+            return
+        end
+
+        set -l paths (string split '\n' -- $selected)
+        set -l count (count $paths)
+
+        echo "Restoring $count file(s):"
+        for p in $paths
+            echo "  $p"
+        end
+
+        read -l -P "Confirm? [y/N] " confirm
+        if string match -qi y -- $confirm
+            for p in $paths
+                echo $p | trash-restore --overwrite
+            end
+        else
+            echo "Aborted."
+        end
+    end
+end
+#
 alias rsud='_rsync --delete'
 alias rsud_d='_rsyncd --delete'
 alias rsu='_rsync'
