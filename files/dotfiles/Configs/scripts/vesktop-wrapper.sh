04255f07eb0b074697e9d3d1f95dddb29a71b4ab
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-03-28 13:39:58 -0600

    Remove deprecated 'vesktop-wrapper.sh'

--- files/dotfiles/Configs/scripts/vesktop-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-WRAPPER="$(command -v chromium-flags.sh 2>/dev/null || echo "")"
-FLATPAK="$(command -v flatpak 2>/dev/null || echo "")"
-FLATPAK_TARGET="dev.vencord.Vesktop"
-
-exec "$WRAPPER" "$FLATPAK" run "$FLATPAK_TARGET" "$@"
